3c5663609df1512bf9c7ef03b8a922d78cb12783
yuanfang <65866693@qq.com>
2020-10-11 16:02:45 +0800

    add css

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-import { Application, Router } from "https://deno.land/x/oak/mod.ts";
+import { Application, Router, send } from "https://deno.land/x/oak/mod.ts";
 
 import { renderFile } from "https://deno.land/x/dejs@0.8.0/mod.ts";
 
@@ -6,16 +6,22 @@ const router = new Router();
 
 const app = new Application();
 
-router.get("/", async (context) => {
-  context.response.body = await renderFile(`${Deno.cwd()}/view/index.ejs`, {
+router.get("/", async (ctx) => {
+  ctx.response.body = await renderFile(`${Deno.cwd()}/view/index.ejs`, {
     title: "Hello Yuanfang",
     author: "yuanfang",
     github: "https://github.com/iyuanfang/deno-ejs",
   });
 });
 
-
 app.use(router.routes());
+
+app.use(async (ctx) => {
+  await send(ctx, ctx.request.url.pathname, {
+    root: `${Deno.cwd()}`,
+  });
+});
+
 const port: number = Number(Deno.env.get("PORT")) || 80;
 
 app.listen({ port });
